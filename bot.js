044fb57a8d32705af3442f0de9da1ffc78938d2c
kolosz <hendrik-ott@gmx.de>
2020-12-06 19:13:08 +0100

    BOT initial commit msg 2

--- bot.js
+++ bot.js
@@ -6,11 +6,27 @@ require('dotenv').config();
 // constants
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const channelName = 'arrivals';
 
 client.login(process.env.BOTTOKEN);
 client.on('ready', readyDiscord);
+client.on('message', gotMessage);
 
 // functions
 function readyDiscord() {
     console.log('go!');
 }
+
+function gotMessage(msg) {
+    console.log(msg);
+    if (msg.channel.id === process.env.CHANNELID && msg.content === 'choo choo') {
+        //msg.reply('motherfucker');
+        msg.channel.send('motherfucker');
+    }
+}
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === channelName);
+    if(!channel) return;
+    channel.send('Welcome you kolosz\'al sweatlord ${member}!');
+});
