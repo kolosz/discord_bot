2898b01b2813a84c16ea65a75515c1b68effdbd4
kolosz <hendrik-ott@gmx.de>
2020-12-21 22:27:07 +0100

    BOT gif implementation

--- bot/bot.js
+++ bot/bot.js
@@ -1,27 +1,45 @@
 console.log('Beep beep!');
 
 // dependencies
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 
 // constants
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const channelName = 'arrivals';
 
-client.login(process.env.BOTTOKEN);
 client.on('ready', readyDiscord);
 client.on('message', gotMessage);
+client.login(process.env.BOTTOKEN);
 
 // functions
 function readyDiscord() {
     console.log('go!');
 }
 
-function gotMessage(msg) {
-    console.log(msg);
-    if (msg.channel.id === process.env.CHANNELID && msg.content === 'choo choo') {
-        //msg.reply('motherfucker');
-        msg.channel.send('motherfucker');
+async function gotMessage(msg) {
+    if (msg.channel.id === process.env.CHANNELID) {
+        let tokens = msg.content.split(' ');
+
+        console.log(msg);
+        if (tokens[0] === "!choochoo") {
+            //msg.reply('motherfucker');
+            //msg.channel.send('motherfucker');
+        } else if (tokens[0] === "!gif" ) {
+
+            let keywords = 'random';
+            if(tokens.length > 1) {
+                keywords = tokens.slice(1, tokens.length).join(" ");
+            }
+
+            let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&contentfilter=low`
+            let response = await fetch(url);
+            let json     = await response.json();
+            console.log(json);
+
+            msg.channel.send(json.results[0].url);
+        }
     }
 }
 
