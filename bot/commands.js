0d7176ddf70d50d6bdded1d658f70649789ead92
kolosz <hendrik-ott@gmx.de>
2022-03-03 08:12:35 +0100

    update packages + wip

--- bot/commands.js
+++ bot/commands.js
@@ -1,11 +1,12 @@
 const gif = require("./../commands/gif.js");
 const basic = require("./../commands/basic.js");
 const bungie = require("./../commands/bungie/bungie.js");
+const info = require("./../commands/info.js");
 
 const commands = { basic, gif, bungie}
 
 // TODO: need to differntiate between gif and bungie command, not depended on the channel maybe
-module.exports= async function (msg) {
+module.exports = async function (msg) {
     if (msg.channel.id === process.env.CHANNELID) {
         let tokens = msg.content.split(" ");
         let command = tokens.shift();
---
+++ commands/info.js
@@ -0,0 +1,3 @@
+module.exports = async function(usr, msg) {
+    console.log('new function coming soon')
+}
